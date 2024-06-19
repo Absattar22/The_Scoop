1d71b98b18a8316f6c62d768275ebc5b58f38d32
Zeyad_Absattar <zeyadabdelsattar@gmail.com>
2024-06-19 16:44:09 +0300

    Finish The UI for the Category View

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -10,6 +10,24 @@ class CategoryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          category,
+          style: const TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 5, 9, 20),
+      ),
       backgroundColor: const Color.fromARGB(255, 5, 9, 20),
       body: CustomScrollView(
         slivers: [
